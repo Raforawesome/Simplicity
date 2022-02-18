5a932013fd184556a1fd49c2859bc741e5f35c99
Raforawesome <ubuntuhasnoroblox@gmail.com>
2022-02-18 00:46:40 -0500

    changed token to main

--- index.ts
+++ index.ts
@@ -24,7 +24,7 @@ for (const cfile of commandFiles) {
 
 // main: NzAyNzY2MTU0NjMyMTM0Njc2.XqEz_A.CeD1wsAzv6oZ349VzkfOyXEfEiM
 // PTB: NzA0MDI2Mzc1NzM2MTMxNTk0.XqXJqA.BKRiCCcMkvfDeXNw1r1mJTd4Q6Q
-client.login('NzA0MDI2Mzc1NzM2MTMxNTk0.XqXJqA.BKRiCCcMkvfDeXNw1r1mJTd4Q6Q');
+client.login('NzAyNzY2MTU0NjMyMTM0Njc2.XqEz_A.CeD1wsAzv6oZ349VzkfOyXEfEiM');
 
 function getRandomInt(min: number, max: number) {
     min = Math.ceil(min);
